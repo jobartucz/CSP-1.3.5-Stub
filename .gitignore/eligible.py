831824584c50499f8ae6538196fe0c31adac39bb
Mr. Bartucz <jobartucz@isd535.org>
2017-10-11 07:55:30 -0500

    added stub file

---
+++ .gitignore/eligible.py
@@ -0,0 +1,6 @@
+def how_eligible(essay):
+    
+    # read the string in the variable essay and return the number equal to the number of these four characters that the essay included.
+    # These are the characters: ?, ", ,, and !
+    
+    return 0
